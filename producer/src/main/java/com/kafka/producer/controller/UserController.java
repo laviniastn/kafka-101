e431537380aca32dcbafe2465a93299d67379ade
laviniastn <laviniastn25@gmail.com>
2021-11-20 22:53:48 +0200

    kafka-p-msg

---
+++ producer/src/main/java/com/kafka/producer/controller/UserController.java
@@ -0,0 +1,26 @@
+package com.kafka.producer.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("kafka")
+public class UserController {
+
+	@Autowired
+	private KafkaTemplate<String, String> kafkaTemplate;
+
+	private static final String TOPIC = "kafka";
+
+	@GetMapping("/publish/{msg}")
+	public String post(@PathVariable("msg") final String name) {
+
+		kafkaTemplate.send(TOPIC, "Message");
+
+		return "Published successfully";
+	}
+}
